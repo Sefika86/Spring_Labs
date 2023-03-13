1c997b72f35e8a8a8337c532eeb2265d59fe92bd
sefika86 <sefikaakin86@gmail.com>
2023-03-13 14:13:16 -0400

    solution 04

---
+++ lab-04-springMVC/src/main/java/com/cydeo/lab04springmvc/controller/ProfileController.java
@@ -0,0 +1,29 @@
+package com.cydeo.lab04springmvc.controller;
+
+import com.cydeo.lab04springmvc.model.Profile;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.time.LocalDateTime;
+
+@Controller
+public class ProfileController {
+
+    @RequestMapping("/profile")
+    public String getProfile(Model model){
+
+        Profile profile = new Profile();
+
+        profile.setName("Harold");
+        profile.setSurname("Finch");
+        profile.setUserName("admin");
+        profile.setPhoneNumber("2023244432");
+        profile.setEmail("admin@cydeo.com");
+        profile.setCreatedDate(LocalDateTime.now());
+
+        model.addAttribute("profile",profile);
+
+
+        return "profile/profile-info";
+    }
+}
